d9a04982e649cac04da17ec1b27f7cf03339e055
James <jamesbosley123@gmail.com>
2019-04-30 22:11:09 -0400

    Changes to my files

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 "use strict";
-let user = undefined;
-let userLife = 40;
-let grantLife = 10;
-let userWs = 0;
-let grantWs = 0;
-let userDmg = 0;
-let grantDmg = 0;
+let wins = 0;
+let userHP = 40;
+let grantHP = 10;
 
-let game = prompt("Would you like to play?", "yes or no");
-if (game === "yes") {
-    user = prompt("Enter your name");
-    while (grantWs < 1 && userWs < 3) {
-        if (userLife > 0 && grantLife > 0) {
-            userDmg = Math.floor((Math.random() * 2) + 1);
-            grantDmg = Math.floor((Math.random() * 2) + 1);
-            userLife = userLife - userDmg;
-            console.log(`${user} has ${userLife} health left`);
-            grantLife = grantLife - grantDmg;
-            console.log(`Grant the almighty chicken has ${grantLife} health left`);
-            if (grantLife <= 0) {
-            userWs++;
-            grantLife = 10;
-            console.log("You beat Grant");
-            } if (userLife <= 0) {
-            grantWs++;
-            console.log("The almighty Grant won game over");
-            break;
-            } if (userWs === 3)
-            console.log("You won the game");
-            break;
+let play = "";
+let name = "";
+let aOq = "";
 
+function startGame() {
+    play = prompt("Would you like to play?");
+    if (play === "yes") {
+        name = prompt("Enter your name.");
+        startCombat();
+    } else {
+        console.log("Goodbye");
+    }
+}
+function getDamage() {
+    userHP -= Math.floor((Math.random() * 5) + 1);
+    grantHP -= Math.floor((Math.random() * 5) + 1);
+}
+
+function startCombat() {
+    while (play) {
+        aOq = prompt("attack or quit");
+        if (aOq === "attack") {
+            getDamage();
+            console.log(`${name} has ${userHP} health left.`);
+            console.log(`Grant has ${grantHP} health left.`);
+            if (grantHP <= 0) {
+                wins++;
+                grantHP = 10;
+                if (wins === 3) {
+                    console.log(`${name} is victorious. ${name} wins the game.`);
+                    break;
+                } else {
+                    console.log(`Grant has been defeated. ${name} has to beat Grant ${3 - wins} more time(s).`);
+                }
+            }
+            if (userHP <= 0) {
+                console.log(`${name} has been defeated. Sucks to suck.`);
+                break;
+            }
+        } else {
+            return;
         }
     }
 }
+
+startGame();
+
